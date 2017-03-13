f9e8f0adf1dd394da9017af1e1b90b90591ea7f9
Edy Segura <ednardo@inatel.br>
2017-03-12 22:37:11 -0300

    Added first algorithm lonely number

---
+++ algorithms/odd-occurences/odd-occurences.js
@@ -0,0 +1,13 @@
+const peerNumbers = [9,3,9,3,7]
+
+function whoIsAlone (numbers) {
+    let lonelyNumbers = {}
+    const total = numbers.length
+    for (let i = 0; i < total; i++) {
+        if (lonelyNumbers[numbers[i]]) delete lonelyNumbers[numbers[i]]
+        else lonelyNumbers[numbers[i]] = numbers[i]
+    }
+    return Object.keys(lonelyNumbers)
+}
+
+console.log(whoIsAlone(peerNumbers))
