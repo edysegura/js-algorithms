ea3c01571bfd0861ad06ca5ba8b72a2dbb01236f
Edy Segura <edysegura@gmail.com>
2019-12-18 14:23:59 -0300

    Refactor shuffle example

--- shuffle/index.js
+++ shuffle/index.js
@@ -1,23 +1,22 @@
+'use strict'
+
 function shuffle(array) {
-  var counter = array.length,
-    temp,
-    index
-  // While there are elements in the array
+  let counter = array.length
+  let temp, index
+
   while (counter > 0) {
-    // Pick a random index
     index = Math.floor(Math.random() * counter)
-    // Decrease counter by 1
     counter--
-    // And swap the last element with it
     temp = array[counter]
     array[counter] = array[index]
     array[index] = temp
   }
+
   return array
 }
 
 const numberList = [1, 2, 3, 4, 5, 6]
-const namesList = ['Edy', 'Lidy', 'Davi']
+const nameList = ['Edy', 'Lidy', 'Davi']
 
 console.log(numberList, shuffle([ ...numberList ]))
-console.log(namesList, shuffle([ ...namesList ]))
+console.log(nameList, shuffle([ ...nameList ]))
