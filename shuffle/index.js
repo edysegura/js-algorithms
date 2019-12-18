33816ff77393fe7e3ce5e36b0b662c74cc4f8176
Edy Segura <edysegura@gmail.com>
2019-12-18 14:23:59 -0300

    Refactor shuffle example

--- shuffle/index.js
+++ shuffle/index.js
@@ -7,9 +7,9 @@ function shuffle(array) {
   while (counter > 0) {
     index = Math.floor(Math.random() * counter)
     counter--
-    temp = array[counter]
-    array[counter] = array[index]
-    array[index] = temp
+    temp = array[ counter ]
+    array[ counter ] = array[ index ]
+    array[ index ] = temp
   }
 
   return array
