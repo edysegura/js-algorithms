72839a8391d19a76192aa0244e6366e6fd25c6ee
Edy Segura <edysegura@gmail.com>
2019-12-19 22:52:00 +0300

    Replace if/else by ternary statement

--- odd-occurences/odd-occurences.js
+++ odd-occurences/odd-occurences.js
@@ -5,8 +5,9 @@ function whoIsAlone (numbers) {
     const total = numbers.length
 
     for (let i = 0; i < total; i++) {
-        if (lonelyNumbers[numbers[i]]) delete lonelyNumbers[numbers[i]]
-        else lonelyNumbers[numbers[i]] = numbers[i]
+        lonelyNumbers[numbers[i]]
+            ? delete lonelyNumbers[numbers[i]]
+            : lonelyNumbers[numbers[i]] = numbers[i]
     }
 
     return Object.keys(lonelyNumbers)
