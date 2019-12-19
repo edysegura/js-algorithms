81e39935daf616856658698b70e4aabeecce543e
Edy Segura <edysegura@gmail.com>
2019-12-19 22:23:00 +0300

    Refactor odd-occurences

--- odd-occurences/odd-occurences.js
+++ odd-occurences/odd-occurences.js
@@ -1,12 +1,14 @@
-const peerNumbers = [9,3,9,3,7]
+const peerNumbers = [ 9,3,9,3,7 ]
 
 function whoIsAlone (numbers) {
     let lonelyNumbers = {}
     const total = numbers.length
+
     for (let i = 0; i < total; i++) {
         if (lonelyNumbers[numbers[i]]) delete lonelyNumbers[numbers[i]]
         else lonelyNumbers[numbers[i]] = numbers[i]
     }
+
     return Object.keys(lonelyNumbers)
 }
 
