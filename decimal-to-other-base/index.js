0cf4f5fb1cad886d4a850dd3546d021905c3ef3f
Edy Segura <edysegura@gmail.com>
2019-12-20 23:11:27 -0300

    Add the initial implementation for decToBase

--- decimal-to-other-base/index.js
+++ decimal-to-other-base/index.js
@@ -1,5 +1,8 @@
 'use strict'
 
 function decToBase(number, base) {
-  
+  return number.toString(base).toUpperCase()
 }
+
+console.log(decToBase(10, 2)) // 1010
+console.log(decToBase(1000, 16)) // 3E8
