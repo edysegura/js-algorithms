3edaa47894a3cb17471a9c05e28b7af705913c33
Edy Segura <edysegura@gmail.com>
2020-11-12 21:43:48 -0300

    Update index.js

--- stack/index.js
+++ stack/index.js
@@ -2,4 +2,19 @@ import { Stack } from './Stack.js'
 
 const stack = new Stack()
 
-console.log(stack.isEmpty())
+stack.push('Davi')
+stack.push('Lidy')
+stack.push('Edy')
+
+console.log('is empty? ', stack.isEmpty())
+console.log(stack.peek())
+console.log(stack.size())
+
+stack.pop()
+
+console.log(stack.peek())
+console.log(stack.size())
+console.log('clear')
+stack.clear()
+console.log(stack.size())
+console.log('is empty? ', stack.isEmpty())
