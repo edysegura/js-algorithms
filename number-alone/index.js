9f9f00a7b67e64682e13908fbb4227eafd900c31
Edy Segura <edysegura@gmail.com>
2020-12-21 22:28:43 -0300

    Add more test cases

--- number-alone/index.js
+++ number-alone/index.js
@@ -1,7 +1,9 @@
 const pairs1 = [1, 2, 2, 3, 3]
+const pairs2 = [5, 5, 6, 6, 4]
 
 function process(pairs) {
   return pairs.reduce((acc, current) => acc ^ current)
 }
 
-console.log(process(pairs))
+console.log(process(pairs1))
+console.log(process(pairs2))
