337ba13e91056055c7928fcda72930843232b7aa
Edy Segura <edysegura@gmail.com>
2021-01-19 09:43:25 -0300

    Document base converter using toString

--- stack/decimal-to-binary.js
+++ stack/decimal-to-binary.js
@@ -1,6 +1,7 @@
 import { Stack } from './Stack.js'
 
 export function decimalToBinary(number) {
+  // return number.toString(2)
   const remainderStack = new Stack()
   let currentNumber = number
   let remainder
