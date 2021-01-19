76047f2bbc45386a120cf82603b2ca06b4d00601
Edy Segura <edysegura@gmail.com>
2021-01-18 22:25:29 -0300

    Add decimal to binary algorithm implementation

--- stack/decimal-to-binary.js
+++ stack/decimal-to-binary.js
@@ -1,5 +1,20 @@
 import { Stack } from './Stack.js'
 
 export function decimalToBinary(number) {
-  return number.toString(2)
+  const remainderStack = new Stack()
+  let currentNumber = number
+  let remainder
+  let binaryString = ''
+
+  while (currentNumber > 0) {
+    remainder = Math.floor(currentNumber % 2)
+    remainderStack.push(remainder)
+    currentNumber = Math.floor(currentNumber / 2)
+  }
+
+  while (!remainderStack.isEmpty()) {
+    binaryString += remainderStack.pop()
+  }
+
+  return binaryString
 }
