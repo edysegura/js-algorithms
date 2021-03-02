a4be06ffa7597105b4e231216eadda9110ec6735
Edy Segura <edysegura@gmail.com>
2021-03-01 22:26:38 -0300

    Add baseConverter implementation

--- stack/base-converter.js
+++ stack/base-converter.js
@@ -1,5 +1,25 @@
 import { Stack } from './Stack.js'
 
 export function baseConverter(decNumber, base) {
-  return [decNumber, base]
+  const remStack = new Stack()
+  const digits = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  let currentNumber = decNumber
+  let remainder
+  let baseString = ''
+
+  if (!(base >= 2 && base <= 36)) {
+    throw RangeError('Out of range, please inform a base between 2 and 36')
+  }
+
+  while (currentNumber > 0) {
+    remainder = Math.floor(currentNumber % base)
+    remStack.push(remainder)
+    currentNumber = Math.floor(currentNumber / base)
+  }
+
+  while (!remStack.isEmpty()) {
+    baseString += digits[remStack.pop()]
+  }
+
+  return baseString
 }
