f4dc2adcea65d59e551cbd1b4884393016dd8969
Edy Segura <edysegura@gmail.com>
2021-03-10 21:38:58 -0300

    Add dequeue implementation

--- queue/queue.js
+++ queue/queue.js
@@ -7,4 +7,18 @@ export class Queue {
     this.#items[this.#count] = element
     this.#count++
   }
+
+  dequeue() {
+    if (this.isEmpty()) {
+      return null
+    }
+    const result = this.#items[this.#lowestCount]
+    delete this.#items[this.#lowestCount]
+    this.#lowestCount++
+    return result
+  }
+
+  isEmpty() {
+    return this.#count - this.#lowestCount === 0
+  }
 }
