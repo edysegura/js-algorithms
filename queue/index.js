42888a69a1ee23bed3564a8ad61ef2f6586cc120
Edy Segura <ednardo.segura@mundiale.com.br>
2021-03-17 07:17:01 -0300

    Use dequeue

--- queue/index.js
+++ queue/index.js
@@ -3,10 +3,19 @@ import { Queue } from './queue.js'
 const queue = new Queue()
 console.log('Is queue empty?', queue.isEmpty())
 
-console.log('Filling queue...')
+console.log('Enqueue...')
 queue.enqueue('Edy')
 queue.enqueue('Lidy')
 queue.enqueue('Davi')
+
+console.log('Is queue empty?', queue.isEmpty())
+console.log('Size:', queue.size())
+console.log('String: ', queue.toString())
+console.log('Peek: ', queue.peek())
+
+console.log('Dequeue...')
+queue.dequeue()
+queue.dequeue()
 
 console.log('Is queue empty?', queue.isEmpty())
 console.log('Size:', queue.size())
