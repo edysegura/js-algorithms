5c150db37c1ced1e598706255d0ce2c9d24405d8
Edy Segura <edysegura@gmail.com>
2021-03-19 21:48:13 -0300

    Add test case for Queue

--- queue/queue.test.js
+++ queue/queue.test.js
@@ -2,8 +2,18 @@ import { Queue } from './queue.js'
 
 describe('Queue', () => {
   it('should return true for an empty queue', () => {
+    // GIVEN
     const queue = new Queue()
+    // THEN
     expect(queue.isEmpty()).toBe(true)
+  })
+
+  it('should return false for an non empty queue', () => {
+    // GIVEN
+    const queue = new Queue()
+    queue.enqueue('Davi')
+    // THEN
+    expect(queue.isEmpty()).toBe(false)
   })
 
 })
