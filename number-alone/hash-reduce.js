17c3f2cd9fddd59cfedb40ee324e5e1836cacc18
Edy Segura <edysegura@gmail.com>
2021-03-25 22:40:47 -0300

    Refactor hash-reduce.js

--- number-alone/hash-reduce.js
+++ number-alone/hash-reduce.js
@@ -1,15 +1,17 @@
 const pairs1 = [1, 2, 2, 3, 3]
 const pairs2 = [5, 5, 6, 6, 4]
 
+function impostorReducer(accumulated, current) {
+  if (accumulated[current]) {
+    delete accumulated[current]
+  } else {
+    accumulated[current] = current
+  }
+  return accumulated
+}
+
 function findImpostor(pairs) {
-  const hash = pairs.reduce((accumulated, current) => {
-    if (accumulated[current]) {
-      delete accumulated[current]
-    } else {
-      accumulated[current] = current
-    }
-    return accumulated
-  }, {})
+  const hash = pairs.reduce(impostorReducer, {})
   return +Object.keys(hash)
 }
 
