1457330d2cb281a82a8f907f4d58c8e1ba25a1d2
Edy Segura <edysegura@gmail.com>
2021-03-30 21:11:23 -0300

    Implement push method to LinkedList

--- linked-list/LinkedList.js
+++ linked-list/LinkedList.js
@@ -8,4 +8,22 @@ export class Node {
 export class LinkedList {
   #count = 0
   #head = null
+
+  push(element) {
+    const node = new Node(element)
+    if (this.#head === null) {
+      this.#head = node
+    } else {
+      let current = this.#head
+      while (current.next !== null) {
+        current = current.next
+      }
+      current.next = node
+    }
+    this.#count++
+  }
+
+  size() {
+    return this.#count
+  }
 }
