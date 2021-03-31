c8996414081df02933fe2c3080172693903a3392
Edy Segura <edysegura@gmail.com>
2021-03-31 08:26:01 -0300

    Add test cases

--- linked-list/LinkedList.test.js
+++ linked-list/LinkedList.test.js
@@ -1,11 +1,22 @@
 import { LinkedList } from './LinkedList.js'
 
 describe('LinkedList', () => {
+  it('should return true for an empty LinkedList', () => {
+    // GIVEN
+    const linkedList = new LinkedList()
+    // THEN
+    expect(linkedList.isEmpty()).toBe(true)
+  })
+
   it('should return the right size for a given LinkedList', () => {
     // GIVEN
     const linkedList = new LinkedList()
     // THEN
     expect(linkedList.size()).toBe(0)
+    // WHEN
+    linkedList.push('Edy')
+    // THEN
+    expect(linkedList.size()).toBe(1)
   })
 
 })
