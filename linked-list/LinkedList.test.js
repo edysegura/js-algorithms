9d6eea89017286cb343f61602d420b0527a637b9
Edy Segura <edysegura@gmail.com>
2021-04-05 21:23:51 -0300

    Add unit test for linkedList

--- linked-list/LinkedList.test.js
+++ linked-list/LinkedList.test.js
@@ -1,4 +1,4 @@
-import { LinkedList } from './LinkedList.js'
+import { LinkedList, Node } from './LinkedList.js'
 
 describe('LinkedList', () => {
   it('should return true for an empty LinkedList', () => {
@@ -17,6 +17,14 @@ describe('LinkedList', () => {
     linkedList.push('Edy')
     // THEN
     expect(linkedList.size()).toBe(1)
+  })
+
+  it('should create a new node', () => {
+    // GIVEN
+    const node = new Node('Davi')
+    // THEN
+    expect(node.element).toBe('Davi')
+    expect(node.next).toBe(null)
   })
 
 })
