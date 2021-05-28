9bf07f77f10b1755447f2157b8b3312c6487e570
Edy Segura <edysegura@gmail.com>
2021-05-27 21:23:43 -0300

    Refactor duplicated occurrences

--- remove-duplicates/index.js
+++ remove-duplicates/index.js
@@ -1,8 +1,12 @@
 /*
-  Remove duplicate occurrences
+  Remove duplicated occurrences
   input: [1,2,1,3,3,5,4,1]
   output: [1,2,3,5,4]
 */
 
+export function removeDuplicated(list) {
+  return [...new Set(input)]
+}
+
 const input = [1,2,1,3,3,5,4,1]
-console.log([...new Set(input)])
+console.log(removeDuplicated(input)) // [ 1, 2, 3, 5, 4 ]
